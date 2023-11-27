0fe4db50e2a8f804c686f7f6722f707a87e6caf6
Annie Nusbaum <annie.nusbaum.83@gmail.com>
2023-11-26 20:22:26 -0500

    BankEncapsulation

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace BankEncapsulation
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var mAcct = new BankAccount(400);
+
+            mAcct.Deposit(30000);
+
+            var bal = mAcct.GetBalance();
+
+            mAcct.Withdraw(300);
+
         }
     }
 }
+
+
+
+//Create a new public class named BankAccount
+//In your BankAccount class, create the following:
+
+//A private field of type double named **balance **with a value of 0
+//Define a method named Deposit that will accept a double and store that value in the balance field
+//Define a method named GetBalance that will return the amount stored in the balance field
+//In the main method of your application, create a new instance of the BankAccount class.
+//Allow the user of the application to Deposit money and retrieve their balance through the console.
+//Once finished save, commit, and push back to Github
